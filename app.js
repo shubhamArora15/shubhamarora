ce1c9378d78c0c75ad5785d97d52b33e9fb70f54
shubhamarora15 <ashubham.1994@gmail.com>
2017-09-23 12:31:59 +0530

    portfolio domain added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var path = require('path');
 var express = require('express');
-
+var router = express.Router();
 /**
  * Creating instances for .
  */
@@ -12,7 +12,7 @@ var app = express();
  * Get port from environment and store in Express.
  */
 
-var port = process.env.PORT || '3000';
+var port = process.env.PORT || '8111';
 app.set('port', port);
 
 /**
@@ -26,10 +26,15 @@ server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
-function onError(){
+function onError(error){
+  console.log(port,error)
   console.log("Problem in starting server");
 }
 
 function onListening(){
   console.log("Server is Started")
 }
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {root: __dirname })
+});
